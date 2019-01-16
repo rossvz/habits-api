defe2a4187d84a22cde6a704a2bf8c007c6f4e74
Ross van Zyl <rossvz@getsellr.com>
2019-01-15 23:35:04 -0500

    deployment stuff

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 require('dotenv').config()
 
-import "reflect-metadata";
-import { createExpressServer } from "routing-controllers";
-import { createConnection } from "typeorm";
+import 'reflect-metadata'
+import { createExpressServer } from 'routing-controllers'
+import { createConnection } from 'typeorm'
 import connectionOptions from './connectionOptions'
 
-const PORT =  8383;
+const PORT = 8383
 
 createConnection(connectionOptions)
   .then(async connection => {
     const app = createExpressServer({
       cors: true,
-      controllers: [__dirname + "/controller/*"],
-      middlewares: [__dirname + "/middleware/*"]
-    });
-    app.listen(PORT);
-    console.log(`Running on port ${PORT}`);
+      controllers: [__dirname + '/controller/*'],
+      middlewares: [__dirname + '/middleware/*']
+    })
+    app.listen(PORT)
+    console.log(`Running on port ${PORT}`)
   })
-  .catch(error => console.log(error));
+  .catch(error => console.log(error))
